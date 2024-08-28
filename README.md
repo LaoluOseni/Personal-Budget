d25fa3430dbbe46ed7e424815d0cea0768353808
Laolu Oseni <63481526+LaoluOseni@users.noreply.github.com>
2024-08-28 08:34:03 -0700

    Specifed Endpoints Working. Needs More Work

---
+++ README.md
@@ -0,0 +1,12 @@
+Personal Budget Project
+
+A porject that helps implement envelope budgeting.
+
+Create, Read, Update and Delete Envelopes. NOT IN ANYWAY COMPLETE. WORK IN PROGRESS!!
+
+Data Structure for Envelopes is preety simple... for now.
+
+Functions to work on envelopes should also be written.
+
+FrontEnd for User Interaction.
+API To spread balance among envelopes. Or Divide among envelopes.
--- envelopes.js
+++ envelopes.js
@@ -2,10 +2,39 @@ const express = require("express");
 const envelopes = require("./data.js");
 const envelopeRouter = express.Router();
 
-envelopeRouter.get("/", (req, res, next) => {
-    res.send(envelopes);
+
+//Param to select envelope by Id
+envelopeRouter.param("id", (req, res, next, id) => {
+    const numId = Number(id);
+    const selectedEnvelope = envelopes.find((env) => {
+        return env.id === numId;
+    });
+    req.selectedEnvelope = selectedEnvelope;
+    //console.log(selectedEnvelope);
+    next();
 })
 
+//Get All Envelopes
+envelopeRouter.get("/", (req, res, next) => {
+    res.send(envelopes);
+});
+
+//Get Envelope By Id
+envelopeRouter.get("/:id", (req, res, next) => {
+    res.send(req.selectedEnvelope);
+})
+
+
+//Update Envelope Budget remaining after spending
+envelopeRouter.post("/:id", (req, res, next) => {
+    const { cost } = req.body;
+    req.selectedEnvelope.budget = req.selectedEnvelope.budget - cost;
+    const updateIndex = envelopes.findIndex(env => env.id === req.selectedEnvelope.id);
+    envelopes[updateIndex] = req.selectedEnvelope;
+    res.send(envelopes[updateIndex]);
+})
+
+//Create Envelope
 envelopeRouter.post("/", (req, res, next) => {
     const { title, budget } = req.body;
     const newid = envelopes.length + 1;
@@ -17,6 +46,44 @@ envelopeRouter.post("/", (req, res, next) => {
     envelopes.push(newEnvelope);
     console.log(envelopes);
     res.send(newEnvelope);
+})
+
+//Delete Envelope
+envelopeRouter.delete("/:id", (req, res, next) => {
+    const updateIndex = envelopes.findIndex(env => env.id === req.selectedEnvelope.id);
+    envelopes.splice(updateIndex, 1);
+    res.send(envelopes);
+})
+
+
+//Transfer routes
+envelopeRouter.param("from", (req, res, next, from) => {
+    console.log(from);
+    const originEnv = envelopes.find(env => env.title === from);
+    const originIndex = envelopes.findIndex(env => env.id === originEnv.id);
+    req.originEnv = originEnv;
+    req.originIndex = originIndex;
+    next();
+})
+
+envelopeRouter.param("to", (req, res, next, to) => {
+    const destEnv = envelopes.find(env => env.title === to);
+    const destIndex = envelopes.findIndex(env => env.id === destEnv.id);
+    req.destEnv = destEnv;
+    req.destIndex = destIndex
+    next();
+})
+
+envelopeRouter.post("/transfer/:from/:to", (req, res, next) => {
+    console.log(req.params);
+    const { amount } = req.body;
+    req.originEnv.budget -= amount;
+    req.destEnv.budget += amount;
+    envelopes[req.originIndex] = req.originEnv;
+    envelopes[req.destIndex] = req.destEnv;
+
+    res.send(envelopes[req.originIndex]);
+    
 })
 
 module.exports = envelopeRouter;
